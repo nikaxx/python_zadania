def1e8602e9c1f4f119b1ddc3401c42f4286cd37
nikaxx <fala502@gmail.com>
2021-01-16 17:10:14 +0100

    zad_2_6

--- petle/zad_2_6.py
+++ petle/zad_2_6.py
@@ -14,3 +14,25 @@ Ala ma <kota>, a kot ma Alę
       - mam przestać zliczać
       - czy zliczanie jest włączone i wtedy zliczam
 """
+warunek = False
+napis = input("Podaj napis:")
+#napis = "Ala ma <kota>, a kot ma Alę"
+ilosc_nawiasow_otwierajacych = napis.count("<")
+ilosc_nawiasow_zamykajacych = napis.count(">")
+if ilosc_nawiasow_otwierajacych == 1 & ilosc_nawiasow_zamykajacych == 1:
+    warunek = True
+print(warunek)
+zliczac = False
+if warunek:
+    napis_wewnetrzny = ""
+    for znak in napis:
+        if znak == ">":
+            zliczac = False
+        if zliczac:
+            napis_wewnetrzny += znak
+        if znak == "<":
+            zliczac = True
+
+    print(len(napis_wewnetrzny))
+
+
